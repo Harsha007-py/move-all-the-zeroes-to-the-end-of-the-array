1b52100d073a7a9cb8944307052528a88815a38d
Harsha007-py <54653816+Harsha007-py@users.noreply.github.com>
2020-08-30 17:10:46 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	int t;
+	cin>>t;
+	while(t-->0){
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    int lp=0,rp=0;
+	    while(rp<n){
+	        if(a[rp]==0){
+	            rp++;
+	        }
+	        else{
+	            int temp=a[lp];
+	            a[lp]=a[rp];
+	            a[rp]=temp;
+	            lp++;
+	            rp++;
+	        }
+	    }
+	    for(int i=0;i<n;i++){
+	        cout<<a[i]<<" ";
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
+//Contibuted by M.Harshavardhan
